03c71b77dc5d44ad3e8ec97db120edb7d6dc4e99
Alexander Bock <mail@alexanderbock.eu>
2020-11-10 16:23:20 +0100

    Finish day 9

---
+++ Intcode/test.bat
@@ -0,0 +1,11 @@
+cd ..\2
+jai day2.jai ..\Intcode\intcode.jai
+
+cd ..\5
+jai day5.jai ..\Intcode\intcode.jai
+
+cd ..\7
+jai day7.jai ..\Intcode\intcode.jai
+
+cd ..\9
+jai day9.jai ..\Intcode\intcode.jai
